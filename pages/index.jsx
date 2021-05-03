000ef0a217f1f49e117d7f1857c50149f5b3acb6
gndclouds <gndclouds@macintosh.local>
2021-05-03 12:55:30 +0000

    backup

--- pages/index.jsx
+++ pages/index.jsx
@@ -39,13 +39,29 @@ const IndexPage = () => {
       <div>
         {!user ? (
           <>
+          <div>
+
+            <img />
+            <h1>A universal micro-blogg, for creators to better main connect with their community</h1>
             <p className="hero-text">
-              Heyo~! This is a small social site. You can submit logs of what
-              you’ve been up to as a casual creative (of any kind). <br />
-              <br />
-              Have a wonderful day.~*
-            </p>
-            <div className="hero-text ascii"></div>
+            It’s really hard to keep your work all in one place, you often ware mant hats and share your work across sites. We give one you place to show all your work from across the internet in one place.
+              </p>
+          </div>
+
+          <div>
+            <h2>What Creators are saying</h2>
+          </div>
+
+          <div>
+          </div>
+
+          <div>
+          </div>
+
+          <div>
+          </div>
+
+          <div className="hero-text ascii"></div>
           </>
         ) : (
           <>
@@ -55,11 +71,12 @@ const IndexPage = () => {
               We are still adding things to the site to please feel free to make
               seggestions in discord.
             </p>
+            <Posts />
+
+
           </>
         )}
 
-        <PostEditor />
-        <Posts />
       </div>
     </>
   );
